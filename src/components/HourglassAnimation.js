639579bfe59743c4338419dd2510f1d518002f68
goncalomp <goncalomrpereira@gmail.com>
2020-07-28 19:26:08 +0100

    minor class fix; added svg files to assets

--- src/components/HourglassAnimation.js
+++ src/components/HourglassAnimation.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import { IonIcon, IonLabel, IonTextarea, IonCol } from "@ionic/react";
-import {
-  hourglass as hourglassIcon,
-} from "ionicons/icons";
+import React from "react";
+import { IonIcon, IonLabel } from "@ionic/react";
+import { hourglass as hourglassIcon } from "ionicons/icons";
 
 function Hourglass({ timeService }) {
   return (
-    <div class="hourglass-component">
+    <div className="hourglass-component">
       <IonLabel>{timeService}h</IonLabel>
-      <IonIcon class="icon" icon={hourglassIcon} />
+      <IonIcon className="icon" icon={hourglassIcon} />
     </div>
   );
 }
