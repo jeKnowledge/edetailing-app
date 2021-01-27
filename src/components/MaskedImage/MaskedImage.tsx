3da66e3eebfc0aa3bba82c57eb9864b9d7f8ed65
mariamachado6 <molimpiadias@gmail.com>
2021-01-27 02:17:42 +0000

    Add Top Masks

--- src/components/MaskedImage/MaskedImage.tsx
+++ src/components/MaskedImage/MaskedImage.tsx
@@ -18,10 +18,16 @@ const MaskedImage = ({
     else return "bottom-image";
   }, [variant]);
 
+  const maskId: string = useMemo(() => {
+    if (variant === "default") return "";
+    else if (variant === "top") return "top-mask";
+    else return "bottom-mask";
+  }, [variant]);
+
   return (
     <div className="image-stack">
       <img className="auto-slider" id={imageId} src={image} alt="" />
-      <img src={mask} alt="" />
+      <img id={maskId} src={mask} alt="" />
     </div>
   );
 };
